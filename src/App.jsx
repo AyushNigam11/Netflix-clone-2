f791b6a70bab8e59ea0d09bb84548aa2d6678684
Ayush Nigam <95609003+AyushNigam11@users.noreply.github.com>
2024-01-04 21:15:59 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+
+import Netflix from "./pages/Netflix";
+import Player from "./pages/player";
+import Signup from "./pages/Signup";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/login" element={<Login/>}  />
+        <Route exact path="/signup" element={<Signup/>}  />
+        <Route exact path="/" element={<Netflix/>}  />
+        <Route exact path="/player" element={<Player />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+export default App
---
+++ src/utils/constants.js
@@ -0,0 +1,2 @@
+export const API_KEY = "9aa63896c9a7b609872600760a3a61f7";
+export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
